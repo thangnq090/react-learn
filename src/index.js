cd890102bf7c554ef7394f51d925455cf92ba18d
thangnq090 <thangnq_2k@hotmail.com>
2018-02-08 11:20:18 +0700

    Event handler

--- src/index.js
+++ src/index.js
@@ -24,14 +24,38 @@ const list = [
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      list
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({list : updatedList});
   }
 
   render() {
     return (
       <div className="App">
-        {list.map(function (item) {
-          return <div>{item.title}</div>;
-        })}
+        {this.state.list.map(item => 
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                <button 
+                  onClick={() => this.onDismiss(item.objectID)}
+                  type="button">
+                  Dismiss
+                </button>
+              </span>
+            </div>
+        )}
       </div>
     )
   }
